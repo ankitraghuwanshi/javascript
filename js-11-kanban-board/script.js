3f52e5c4e15c86d957e49d9ca7bd0b1ec66fdb4f
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-06-28 00:05:26 +0530

    update

--- js-11-kanban-board/script.js
+++ js-11-kanban-board/script.js
@@ -1,8 +1,12 @@
 //1.modal popup with toggle
 const addBtn=document.querySelector(".add-btn")
 const modalCont=document.querySelector(".modal-cont")
+const mainCont=document.querySelector(".main-cont")
+const textArea=document.querySelector(".textArea-cont")
+
 //let over const
 let addTaskFlag=false
+
 addBtn.addEventListener('click',function(){
     //sawp the addTaskFlag
     addTaskFlag = !addTaskFlag
@@ -12,5 +16,38 @@ addBtn.addEventListener('click',function(){
     }else{
         //hide modal-cont
         modalCont.style.display="none"
+    }
+})
+
+//function to add a new ticket/task
+function createTicket(){
+    //create a new ticket container element
+    const ticketCont=document.createElement("div")
+
+    //set class as ticket-cont
+    ticketCont.setAttribute("class","ticket-cont")
+
+    //provide innerHtml to ticketCont
+    ticketCont.innerHTML=`
+        <div class="ticket-color"></div>
+        <div class="ticket-id">123456</div>
+        <div class="task-area">text area</div>
+        <div class="ticket-lock">
+            <i class="fa-solid fa-lock"></i>
+        </div>
+    `
+
+    mainCont.appendChild(ticketCont)
+}
+
+modalCont.addEventListener('keydown',function(e){
+    //if we press shift button on modal then ticket generate
+    if(e.key=='Shift'){
+        createTicket()
+        //modal hide
+        modalCont.style.display="none"
+        addTaskFlag=!addTaskFlag
+        //textarea as empty
+        textArea.value=""
     }
 })
