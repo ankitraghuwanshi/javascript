44ff6a0ca8e9389a3f9a012d3fc39a21fb151e25
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-07-04 10:42:20 +0530

    rating

--- eventDelegation/p2/script.js
+++ eventDelegation/p2/script.js
@@ -1,19 +1,42 @@
-const star=document.querySelectorAll(".star")
+//my approach
+const starsParent=document.querySelector("#stars")
+const stars=document.querySelectorAll(".star")
 const rating=document.querySelector("#rating")
 
-star.forEach(function(elem){
-    elem.addEventListener('click',function(e){
-        const dataValue=e.target.getAttribute("data-value")
-        for(let i=0;i<dataValue;i++){
-            star[i].classList.add("filled")
-        }
-        for(let i=dataValue;i<5;i++){
-            if(star[i].classList.contains("filled")){
-                star[i].classList.remove("filled")
+starsParent.addEventListener('click',function(e){
+    const dataValue=e.target.getAttribute("data-value")
+    stars.forEach(function(star){
+        const currentValue=star.getAttribute("data-value")
+        if(currentValue<=dataValue){
+            star.classList.add("filled")
+        }else{
+            if(star.classList.contains("filled")){
+                star.classList.remove("filled")
             }
         }
-        rating.innerText=dataValue
     })
+    rating.innerText=dataValue
 })
 
-//if we click not on star, then all star become colorless
+
+//sir approach
+// const starsParent = document.getElementById('stars')
+// const stars = document.querySelectorAll('.star')
+// const ratingDisplay = document.getElementById('rating')
+// starsParent.addEventListener('click', (e) => {
+//     const star = e.target
+//     const value = star.getAttribute('data-value')
+//     updateValue(value)
+// })
+// function updateValue(value) {
+//     // Loop over all stars 
+//     // And add appropriate class i.e filled
+//     stars.forEach((starElem) => {
+//         const currentValue = starElem.getAttribute('data-value')
+//         starElem.classList.toggle(
+//             "filled",
+//             parseInt(currentValue, 10) <= parseInt(value, 10)
+//         )
+//     })
+//     ratingDisplay.innerText = value
+// }
