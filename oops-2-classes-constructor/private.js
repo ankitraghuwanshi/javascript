2069ac350750fb777c0977633d4ddfd8a5e18fb4
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-07-07 18:48:23 +0530

    initial

---
+++ oops-2-classes-constructor/private.js
@@ -0,0 +1,17 @@
+class Pizza{
+    //private properties
+    #secretIngredient
+    constructor(topping,size,crust){
+        this.topping=topping;
+        this.size=size;
+        this.crust=crust;
+        this.#secretIngredient="pineapple"
+    }
+    describe(){
+        console.log(`this pizza has size "${this.size}" and "${this.crust}" crust with "${this.topping}" topping`)
+    }
+}
+const pizza1=new Pizza("capsicum","M","thin")
+const pizza2=new Pizza(["capsicum","paneer"],"M","thin")
+console.log(pizza1.#secretIngredient)    //syntaxError
+
