c703678697ccd643531a863be8eadc766e75f3f9
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-07-11 20:14:32 +0530

    final

--- js-22-promise-polyfill/promiseAllSettledPolyfill.js
+++ js-22-promise-polyfill/promiseAllSettledPolyfill.js
@@ -1,1 +1,7 @@
 //homework
+
+// Homework, given a list/array of promises,
+// implement a function which triggers these promises 1-by-1,
+//  when the first promise gets resolved,
+//  only then trigger the second promise....if there's any rejection in this chain,
+//  then immediately reject
