1721752d3ce850af5dee86983a23a5c2b0c69e6e
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-07-12 11:07:47 +0530

    final

--- js-22-promise-polyfill/promiseAllSettledPolyfill.js
+++ js-22-promise-polyfill/promiseAllSettledPolyfill.js
@@ -1,5 +1,25 @@
 //homework
-
+//{ status: 'fulfilled', value: 'fetch-product-data resolved' }
+//{ status: 'rejected', reason: 'fetch-profile-data resolved' }
+Promise.myall =function(promises){
+    return new Promise((res,rej)=>{
+        const resultArray=[]  
+        let total=0   //count how many promises resolved
+        promises.forEach((promise,index)=>{
+            Promise.resolve(promise)
+            .then((val)=>{
+                resultArray[index]={ status: 'fulfilled', value: val }
+                total=total+1
+            }).catch((e)=>{
+                resultArray[index]={ status: 'rejected', reason: e }
+                total=total+1
+            })
+            if(total===promises.length){
+                res(resultArray)
+            }
+        })
+    })
+}
 // Homework, given a list/array of promises,
 // implement a function which triggers these promises 1-by-1,
 //  when the first promise gets resolved,
